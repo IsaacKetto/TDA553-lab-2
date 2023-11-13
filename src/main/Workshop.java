58ff55be1dfe0bd47b8a1a99fd0d8d02ddbefe77
IsaacKetto <95360625+IsaacKetto@users.noreply.github.com>
2023-11-13 13:14:01 +0100

    Workshop

---
+++ src/main/Workshop.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class Workshop<T> {
+
+    private int maxNrCars;
+    private int totalCars = 0;
+    private ArrayList<T> cars = new ArrayList<T>();
+    public Workshop(int amount) {
+        maxNrCars = amount;
+    }
+
+    public void add(T car) {
+        if ((totalCars + 1) <= maxNrCars) {
+            totalCars += 1;
+            cars.add(car);
+        }
+    }
+
+
+    private T getVehicle(int index) {
+        if (index >= 0 && index < totalCars) {
+            return cars.get(index);
+        } else {
+            return null;
+        }
+    }
+
+    public T getCar(int index) {
+        T car = getVehicle(index);
+        cars.remove(index);
+        return car;
+    }
+
+}
